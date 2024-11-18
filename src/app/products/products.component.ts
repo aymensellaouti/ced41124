8b20d15d43b84405faf9431c8f9adcaea34cefe4
aymensellaouti <aymen.sellaouti@gmail.com>
2024-11-18 16:01:47 +0100

    sprint 2 product

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -20,11 +20,24 @@ import { Settings } from './dto/product-settings.dto';
 })
 export class ProductsComponent {
   setting: Settings = { limit: 12, skip: 0 };
+  settings$ = new BehaviorSubject<Settings>(this.setting);
   products: Product[] = [];
-  products$!: Observable<Product[]>;
+  products$: Observable<Product[]> = this.settings$.pipe(
+    // setiing1 setting2
+    // 0 12   12 12  12 24 ....
+    concatMap(setting => this.productService.getProducts(setting)),
+    map(apiResponse => apiResponse.products)
+
+  );
   constructor(private productService: ProductService) {}
   /**
    * Get more products
    */
-  more() {}
+  more() {
+    this.setting = {
+      ...this.setting,
+      skip: this.setting.skip + 12
+    };
+    this.settings$.next(this.setting);
+  }
 }
